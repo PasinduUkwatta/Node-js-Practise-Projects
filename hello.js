6f804cebc325324e046e57b369ad34f952e65d4c
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-01-19 09:56:22 +0530

    Add files via upload

--- hello.js
+++ hello.js
@@ -7,3 +7,21 @@ else {
     console.log(`is ${mission} really more fun ?`)
 }
 
+const EventEmitter =require("events")
+const celebrity = new EventEmitter()
+
+celebrity.on("race win",function () {
+    console.log("congratulations !")
+})
+
+celebrity.on("race win",function () {
+    console.log("i could have done better than that !")
+})
+
+process.on("exit",(code => {
+    console.log("process exit event with code ",code)
+}))
+
+celebrity.emit("race win")
+celebrity.emit("race lost")
+celebrity.emit("race win")
