63713c9145423f7dc3a22d8522c57af989b9cf3a
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-02-01 09:12:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
-const { parse } = require("csv-parse");
-const fs = require("fs");
+const http = require("http");
 
-const results = [];
-const habitablePlanets = [];
+const PORT = 3000;
+const server = http.createServer();
 
-function isHabitablePlanet(planet) {
-  return (
-    planet["koi_disposition"] === "CONFIRMED" &&
-    planet["koi_insol"] > 0.36 &&
-    planet["koi_insol"] < 1.11 &&
-    planet["koi_prad"] < 1.6
-  );
-}
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
 
-fs.createReadStream("kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
+  {
+    id: 1,
+    name: "Issac Newton",
+  },
+];
+
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("Request :", friend);
+      friends.push(JSON.parse(friend));
+      req.pipe(res);
+    });
+  } else if (req.method === "GET" && items[1] === "friends") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
     }
-    results.push(data);
-  })
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(`found ${habitablePlanets.length} habitable planets`);
-    console.log(
-      habitablePlanets.map((planet) => {
-        return planet["kepler_name"];
-      })
-    );
-    console.log("done");
-  });
+  } else if (req.method === "GET" && items[1] === "messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hi Pasindu</li>");
+    res.write("<li>How are you ? </li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  }
+});
+
+//127.0.0.1=>localhost
+
+server.listen(PORT, () => {
+  console.log(`listening on port : ${PORT} ....`);
+});
+
+//sample request
+
+// fetch("http://localhost:3000/friends", {
+//   method: "POST",
+//   body: JSON.stringify({ id: 3, name: "Ryan Dhal" }),
+// });
