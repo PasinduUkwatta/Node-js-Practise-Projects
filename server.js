faddf28a8a3f719be1ab7efa74f948339d734345
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2022-03-11 08:47:22 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,59 +1,16 @@
-const express = require("express");
+const http =require("http")
 
-const app = express();
+const app =require("./app")
 
-const PORT = 3000;
+const PORT= process.env.PORT|| 8000
 
-const friends = [
-  {
-    id: 1,
-    name: "pasindu",
-  },
-  {
-    id: 2,
-    name: "thiwanka",
-  },
-  {
-    id: 3,
-    name: "deshan",
-  },
-];
 
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
+app.listen()
+const server =http.createServer(app)
 
-app.get("/friends/:friendId", (req, res) => {
-  const friendId = Number(req.params.friendId);
+server.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}...`)
+})
 
-  const friend = friends[friendId];
 
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({
-      error: "Friend does not exist!",
-    });
-  }
-});
 
-app.get("/", (req, res) => {
-  res.send({
-    id: 1,
-    name: "thiwanka",
-  });
-});
-
-app.get("/messages", (req, res) => {
-  res.send("<ul><li>Hello Pasindu !</li></ul>");
-});
-
-app.post("/messages", (req, res) => {
-  console.log("Updating Messages");
-});
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}...`);
-});
-
-app.use(function (req, res, next) {});
